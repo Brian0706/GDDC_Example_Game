4d647288940ca42bf5247666ae53180fbbc18af3
Brian Chau <brianchau0729@gmail.com>
2025-06-20 11:03:53 -0400

    Added in an animation for the lucky box

--- new-game-project/Code/item_box.gd
+++ new-game-project/Code/item_box.gd
@@ -1,7 +1,11 @@
 extends Area2D
+@export
 var item: String
+@export
+var animation: AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	print("got item!")
+	animation.play("GetItem")
 	if (body.name == "Player" && item != ""):
 		print("got item!")
