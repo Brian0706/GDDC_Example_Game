d0703474978e13e0a35b99be5c3cb7e6f0ab8326
Brian Chau <brianchau0729@gmail.com>
2025-06-20 14:22:41 -0400

    Made a consumable powerup and made the powerup

--- new-game-project/Code/item_box.gd
+++ new-game-project/Code/item_box.gd
@@ -1,11 +1,13 @@
 extends Area2D
 @export
-var item: String
+var item: PackedScene
 @export
 var animation: AnimationPlayer
-
 func _on_body_entered(body: Node2D) -> void:
-	print("got item!")
-	animation.play("GetItem")
-	if (body.name == "Player" && item != ""):
+	if (body.name == "Player" && item != null):
+		animation.play("GetItem")
+		var powerUP = item.instantiate();
+		item = null
+		add_child(powerUP)
+		powerUP.position.y -= 20
 		print("got item!")
---
+++ new-game-project/Code/special_item.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var direction = 1
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if is_on_wall():
+		direction *= -1
+	velocity.x = direction * SPEED
+
+	move_and_slide()
+
+func consumed():
+	self.queue_free();
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body.name == "Player"):
+		print("has been activated")
+		call_deferred("consumed")
