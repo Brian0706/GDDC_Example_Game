92d5fbe86fc73ce6b3e2c8b4b93db69eabf0d243
Brian Chau <brianchau0729@gmail.com>
2025-08-05 14:16:30 -0400

    boss jumps in direction of player

--- new-game-project/Code/Enemies/boss.gd
+++ new-game-project/Code/Enemies/boss.gd
@@ -4,31 +4,33 @@ extends Enemy
 const JUMP_VELOCITY = -300
 
 func jumping_attack(delta: float):
-    print("attacking")
-    velocity.y = JUMP_VELOCITY
-    jumpCooldown.wait_time = randf_range(3, 4)
-    jumpCooldown.start()
-    curState = 0
-    print(velocity)
-    move_and_slide()
+	direction = int(sign(Global.player.position.x - self.position.x))
+	velocity.y = JUMP_VELOCITY
+	velocity.x = SPEED * direction
+	self.scale.x = direction * -1
+	jumpCooldown.wait_time = randf_range(3, 4)
+	jumpCooldown.start()
+	curState = 0
+	print(velocity)
+	move_and_slide()
 
 func walking(delta: float):
-    # Add the gravity.
-    if not is_on_floor():
-        velocity += get_gravity() * delta
-    velocity.x = direction * SPEED
-    if wallCheck.is_colliding():
-        direction *= -1
-        #By negating the scale we can flip the entire scene, include the raycast, not just the animation
-        self.scale.x *= -1
-    
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	velocity.x = direction * SPEED
+	if wallCheck.is_colliding():
+		direction *= -1
+		#By negating the scale we can flip the entire scene, include the raycast, not just the animation
+		self.scale.x *= -1
+	
 	# ANIMATION SECTION
-    animations.play("walk")
-    move_and_slide()
+	animations.play("walk")
+	move_and_slide()
 
 func _on_jump_attack_timer_timeout() -> void:
-    curState = 2
+	curState = 2
 
 func _ready() -> void:
-    super._ready()
-    states.append(Callable(self, "jumping_attack"))
+	super._ready()
+	states.append(Callable(self, "jumping_attack"))
