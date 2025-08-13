09d49b6d0708ad3a9fb7859916c131ea2bb115c4
Brian Chau <brianchau0729@gmail.com>
2025-08-13 15:22:45 -0400

    Added sound to game over screen

--- new-game-project/Code/game_over.gd
+++ new-game-project/Code/game_over.gd
@@ -1,21 +1,27 @@
 extends Control
+@onready var lostSound: AudioStreamPlayer2D = $GameLost
+@onready var buttonPress: AudioStreamPlayer2D = $ButtonPress
+@onready var transitionTimer: Timer = $Timer
 
+var newScene = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	lostSound.play()
 
 func _on_retry_button_pressed() -> void:
 	Global.reset_game_state()
-	get_tree().change_scene_to_file("res://Scenes/Levels/level-1.tscn")
+	newScene = "res://Scenes/Levels/level-1.tscn"
+	buttonPress.play()
+	transitionTimer.start()
 	print("Rety button pressed")
 
 func _on_main_menu_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	newScene = "res://Scenes/main_menu.tscn"
+	transitionTimer.start()
+	buttonPress.play()
 	print("Main menu button pressed")
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file(newScene)
