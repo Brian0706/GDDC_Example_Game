52a38646bb294d62c780faef149dd4fcdbbafb99
Santhosh <santhoshs0305@gmail.com>
2025-10-27 14:47:48 -0400

    removed UI for demo + added level template screenshot

--- new-game-project/Code/one_up.gd
+++ new-game-project/Code/one_up.gd
@@ -3,7 +3,6 @@ extends PowerUP
 
 func _ready() -> void:
 	print($"/root/Controller/CanvasLayer/HUD")
-	powerup_collected.connect($"/root/Controller/CanvasLayer/HUD".updateLivesLabel)
 
 func consume():
 	Global.add_lives(1)
