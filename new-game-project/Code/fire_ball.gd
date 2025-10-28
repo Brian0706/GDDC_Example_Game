9374a4ea1d9e09dbdc0ff5bb889a44ed5107773f
Santhosh <santhoshs0305@gmail.com>
2025-10-28 03:44:07 -0400

    bruh

--- new-game-project/Code/fire_ball.gd
+++ new-game-project/Code/fire_ball.gd
@@ -1,8 +1,8 @@
 extends PowerUP
 
 func _ready() -> void:
-    super._ready()
-    powerup_collected.connect(Global._give_projectile)
+	super._ready()
+	powerup_collected.connect(Global._give_projectile)
 
 func consume() -> void:
-    super.consume()
+	super.consume()
